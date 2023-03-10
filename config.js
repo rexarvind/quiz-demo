7150ff3a3124611b4b2c87593d29d121b3ceb4f5
Arvind Kumar <thatisarvind@gmail.com>
2023-03-10 19:56:30 +0530

    update config

---
+++ config.js
@@ -0,0 +1,12 @@
+
+var firebaseConfig={
+apiKey:"AIzaSyAIBhvdbyFlTqZLgtNA7uTWHymjHOpzyMU",
+authDomain:"rexarvind.firebaseapp.com",
+databaseURL:"https://rexarvind.firebaseio.com",
+projectId:"rexarvind",
+storageBucket:"rexarvind.appspot.com",
+messagingSenderId:"685927662051",
+appId:"1:685927662051:web:252add00d42a851bc320d6",
+measurementId:"G-CCNCD2RK1F"}
+
+const ROOT_URL="https://client90.000webhostapp.com"
--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-var firebaseConfig={
-apiKey:"AIzaSyAIBhvdbyFlTqZLgtNA7uTWHymjHOpzyMU",
-authDomain:"rexarvind.firebaseapp.com",
-databaseURL:"https://rexarvind.firebaseio.com",
-projectId:"rexarvind",
-storageBucket:"rexarvind.appspot.com",
-messagingSenderId:"685927662051",
-appId:"1:685927662051:web:252add00d42a851bc320d6",
-measurementId:"G-CCNCD2RK1F"}
+
 firebase.initializeApp(firebaseConfig)
 const auth=firebase.auth()
 
-const ROOT_URL="https://client90.000webhostapp.com"
 const ADD_USER=ROOT_URL+"/api/quiz/add-user.php"
 const COUNT_HIGH_SCORE=ROOT_URL+"/api/quiz/count-high-score.php"
 const LIMIT_HIGH_SCORE=ROOT_URL+"/api/quiz/limit-high-score.php"
